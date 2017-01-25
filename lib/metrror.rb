f6dbcdccf8b9267bf25a31c12a43589d03434a90
oesgalha <oscaresgalha@gmail.com>
2017-01-25 13:21:33 -0200

    Initial gem skeleton

---
+++ lib/metrror.rb
@@ -0,0 +1,5 @@
+require 'metrror/version'
+
+module Metrror
+  # Your code goes here...
+end
---
+++ test/metrror_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class MetrrorTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Metrror::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end

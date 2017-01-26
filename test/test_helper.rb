0578c7b9848750b0a30f5328dffe9970c41a1b09
oesgalha <oscaresgalha@gmail.com>
2017-01-26 17:46:18 -0200

    Fix test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
+require 'simplecov'
+SimpleCov.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'metrorb'
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'simplecov'
-SimpleCov.start
